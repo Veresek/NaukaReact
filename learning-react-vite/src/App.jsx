8690cd3968cceb68e6b92ef6071eb1fca9e175a8
Veresek <filipowskipraca@gmail.com>
2025-04-13 23:53:48 +0200

    hgfhf

--- learning-react-vite/src/App.jsx
+++ learning-react-vite/src/App.jsx
@@ -1,24 +1,23 @@
 import { useState } from 'react';
 
-function MyButton() {
-	const [count, setCount] = useState(10);
-	const essa = 3;
-	function handleClick() {
-		setCount(count + essa);
-	}
+export default function Board() {
 	return (
 		<>
-			<button onClick={handleClick}>Clicked {count}</button>
-		</>
-	);
-}
-
-export default function App() {
-	return (
-		<>
-			<MyButton />
-			<br />
-			<MyButton />
+			<div className='board-row'>
+				<button className='square'>1</button>
+				<button className='square'>2</button>
+				<button className='square'>3</button>
+			</div>
+			<div className='board-row'>
+				<button className='square'>4</button>
+				<button className='square'>5</button>
+				<button className='square'>6</button>
+			</div>
+			<div className='board-row'>
+				<button className='square'>7</button>
+				<button className='square'>8</button>
+				<button className='square'>9</button>
+			</div>
 		</>
 	);
 }
