73a7913f4d61d5c901f75fdaa2a0dfa7baaa0e64
Veresek <filipowskipraca@gmail.com>
2025-04-23 00:38:00 +0200

    gfdgd

--- todo-app/src/Sidebar.jsx
+++ todo-app/src/Sidebar.jsx
@@ -17,16 +17,21 @@ function Categories({ catNames }) {
 export default function Sidebar() {
 	const [catNames, setCatNames] = useState(['Szkoła', 'Praca', 'Dom']);
 	return (
-		<>
-			<div className='relative'>
+		<div className='px-3 pt-8 pb-5'>
+			<div className='relative '>
 				<input
 					type='text'
 					placeholder='Szukaj'
-					className='block bg-search-background placeholder-white/60 text-white px-6 py-4 text-[14.5px]'
+					className='block bg-search-background placeholder-white/60 text-white px-2 py-4 pl-10 text-[16px] rounded-xl'
 				/>
-				<img src={SearchBtn} alt='search icon' className='absolute bottom-4' />
+				<img
+					src={SearchBtn}
+					alt='search icon'
+					className='absolute bottom-4.5 left-[12px]'
+				/>
 			</div>
+			<p className='text-white text-xl'>Kategorie</p>
 			<Categories catNames={catNames} />
-		</>
+		</div>
 	);
 }
