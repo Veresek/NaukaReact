57566c39e412404f98c9a3989c98fb41ff646d10
Veresek <filipowskipraca@gmail.com>
2025-05-07 01:16:01 +0200

    Added style and functionality to priority section

--- todo-app/src/components/task/CreateTask.tsx
+++ todo-app/src/components/task/CreateTask.tsx
@@ -8,28 +8,28 @@ import closeIcon from '@assets/closeIcon.png';
 function ImportanceTab({
 	imgSrc,
 	label,
-	isSet,
+	id,
+	activeTab,
+	setActiveTab,
 }: {
 	imgSrc: string;
 	label: string;
-	isSet: boolean;
+	id: string;
+	activeTab: string;
+	setActiveTab: (isActive: string) => void;
 }) {
-	const [isChecked, setIsChecked] = useState(false);
-	if (isChecked) {
-		return (
-			<button className='flex items-center bg-main-section'>
-				<img src={imgSrc} className='mr-2' />
-				<p>{label}</p>
-			</button>
-		);
-	} else {
-		return (
-			<button className='flex items-center '>
-				<img src={imgSrc} className='mr-2' />
-				<p>{label}</p>
-			</button>
-		);
-	}
+	return (
+		<button
+			className={
+				'flex items-center px-3 py-3 rounded-xl cursor-pointer ' +
+				(activeTab === id ? 'bg-main-section' : '')
+			}
+			onClick={() => setActiveTab(id)}
+			id={id}>
+			<img src={imgSrc} className='mr-2' />
+			<p>{label}</p>
+		</button>
+	);
 }
 
 export default function CreateTask({
@@ -37,10 +37,11 @@ export default function CreateTask({
 }: {
 	setIsClicked: Function;
 }) {
+	const [activeTab, setActiveTab] = useState('low');
 	return (
 		<div className='absolute top-0 left-0 w-[100vw] h-[100vh] bg-black/50 backdrop-blur-sm'>
 			<div className='absolute top-[50%] left-[50%] bg-creating-task-background -translate-1/2 text-white py-8 px-6 rounded-2xl border-task-border border min-w-[450px]'>
-				<h2 className='text-2xl mb-10'>Dodaj nowe zadanie</h2>
+				<h2 className='text-2xl mb-10 font-semibold'>Dodaj nowe zadanie</h2>
 				<img
 					src={closeIcon}
 					alt='close button'
@@ -65,12 +66,30 @@ export default function CreateTask({
 						className='px-6 py-4 w-full bg-task-background rounded-xl h-[104px] break-all resize-none'
 					/>
 				</div>
-				<div>
-					<p>Priorytet</p>
-					<div className='flex'>
-						<ImportanceTab imgSrc={highImportance} label='Wysoki' />
-						<ImportanceTab imgSrc={midImportance} label='Średni' />
-						<ImportanceTab imgSrc={lowImportance} label='Niski' />
+				<div className='inline-block mb-10'>
+					<p className='mb-2 px-2'>Priorytet</p>
+					<div className='flex bg-task-background py-1 px-1 rounded-2xl'>
+						<ImportanceTab
+							imgSrc={lowImportance}
+							label='Niski'
+							id='low'
+							activeTab={activeTab}
+							setActiveTab={setActiveTab}
+						/>
+						<ImportanceTab
+							imgSrc={midImportance}
+							label='Średni'
+							id='mid'
+							activeTab={activeTab}
+							setActiveTab={setActiveTab}
+						/>
+						<ImportanceTab
+							imgSrc={highImportance}
+							label='Wysoki'
+							id='high'
+							activeTab={activeTab}
+							setActiveTab={setActiveTab}
+						/>
 					</div>
 				</div>
 				<div className='flex content-end w-full justify-end'>
